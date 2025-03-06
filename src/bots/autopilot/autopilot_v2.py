2461efdfdff24c897cc64974ba534f93f4fed76b
Fekete-Szabó Illés <47056095+Phextod@users.noreply.github.com>
2025-03-06 17:22:58 +0100

    autopilot v2

---
+++ src/bots/autopilot/autopilot_v2.py
@@ -0,0 +1,14 @@
+from src.eve_ui.eve_ui import EveUI
+
+
+class Autopilot:
+    def __init__(self, ui):
+        self.ui: EveUI = ui
+
+    def run(self):
+        self.ui.route.autopilot(self.ui.station_window, self.ui.timers)
+
+
+if __name__ == '__main__':
+    autopilot = Autopilot(EveUI())
+    autopilot.run()
